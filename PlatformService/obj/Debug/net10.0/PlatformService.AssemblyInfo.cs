6ee6dc2f65e418a5c435be7715965b17808f4ed8
KyleCagayan <43043797+KyleCagayan@users.noreply.github.com>
2025-12-29 20:13:57 -0500

    create repo

--- PlatformService/obj/Debug/net10.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net10.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6652808377af1d94e556da2bb6d43808c7311a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c95590cadabda4388973978d342cd8b93e121f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
