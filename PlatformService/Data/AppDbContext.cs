11c95590cadabda4388973978d342cd8b93e121f
KyleCagayan <43043797+KyleCagayan@users.noreply.github.com>
2025-12-29 19:44:57 -0500

    add data objects and interface

---
+++ PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+            
+        }
+
+        // define db set
+        public DbSet<Platform> Platforms { get; set; }
+         
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 
 var app = builder.Build();
 
